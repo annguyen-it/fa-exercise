e8add959ee800966856e572dcc531fd514d68d1a
dattte <72026353+dattte@users.noreply.github.com>
2021-06-30 17:18:35 +0700

    optimize-code

--- travel-blog_Jun-28/script.js
+++ travel-blog_Jun-28/script.js
@@ -6,32 +6,29 @@ let listMenuItemData;
 fetch('https://app-travel-api.herokuapp.com/list-menu')
   .then((response) => response.json())
   .then((data) => (listMenuItemData = data.data))
-  .then(() => loadSidebar())
-  .then(async () => await loadGallery())
-  .then(() => playBackgroundVideo());
+  .then(() => loadData());
 
-function loadSidebar() {
-  var htmls = listMenuItemData.map((menu) => {
-    return `<li><a href="#"></a>${menu.name}</li>`;
-  });
-  var html = htmls.join('');
-  document.querySelector('.list-items').innerHTML = html;
-}
-
-async function loadGallery() {
+async function loadData() {
+  let html =''
   const requestId = listMenuItemData[currentTabIndex].id;
 
   console.log(`https://app-travel-api.herokuapp.com/list-travel/${requestId}`);
 
-  await fetch(`https://app-travel-api.herokuapp.com/list-travel/${requestId}`)
+  fetch(`https://app-travel-api.herokuapp.com/list-travel/${requestId}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.data);
-
-      data.data.forEach((item, index) => {
-        galleryItems[index].querySelector('img').setAttribute('src', item.thumb);
-        galleryItems[index].querySelector('.card-text').innerText = item.title;
+      data.data.forEach((item) => {
+        html += `<div class="col-xl-3 col-md-6">
+          <div class="card gallery-item" style="width: 18rem;">
+            <img src="${item.thumb}" class="card-img-top" alt="travel-picture">
+            <div class="card-body">
+              <p class="card-text">${item.title}</p>
+            </div>
+          </div>
+        </div>`
       });
+
+      document.querySelector('#gallery').innerHTML = html
     });
 }
 
@@ -41,13 +38,9 @@ function changeTab(index) {
   currentTabIndex = index;
 }
 
-function playBackgroundVideo() {
-  document.querySelector('#heroVideo').src += '&autoplay=1';
-}
-
 sidebarItems.forEach((e, index) => {
   e.addEventListener('click', async () => {
     changeTab(index);
-    await loadGallery();
+    await loadData();
   });
 });
