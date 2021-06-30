c5254dcbdcd961f9a5ae03e582f3d71ee4fbf159
An Nguyen <annguyen.it.utc@gmail.com>
2021-06-30 12:31:03 +0700

    Auto play background video after gallery loaded

--- travel-blog_Jun-28/script.js
+++ travel-blog_Jun-28/script.js
@@ -6,23 +6,28 @@ let listMenuItemData;
 fetch('https://app-travel-api.herokuapp.com/list-menu')
   .then((response) => response.json())
   .then((data) => (listMenuItemData = data.data))
-  .then((menus) => {
-      var htmls = menus.map( menu => {
-      return `<li><a href="#"></a>${menu.name}</li>`
-      })
-      var html = htmls.join('')
-      document.querySelector('.list-items').innerHTML = html;
-    })
-  .then(() => loadData());
+  .then(() => loadSidebar())
+  .then(async () => await loadGallery())
+  .then(() => playBackgroundVideo());
 
-async function loadData() {
+function loadSidebar() {
+  var htmls = listMenuItemData.map((menu) => {
+    return `<li><a href="#"></a>${menu.name}</li>`;
+  });
+  var html = htmls.join('');
+  document.querySelector('.list-items').innerHTML = html;
+}
+
+async function loadGallery() {
   const requestId = listMenuItemData[currentTabIndex].id;
 
   console.log(`https://app-travel-api.herokuapp.com/list-travel/${requestId}`);
 
-  fetch(`https://app-travel-api.herokuapp.com/list-travel/${requestId}`)
+  await fetch(`https://app-travel-api.herokuapp.com/list-travel/${requestId}`)
     .then((response) => response.json())
     .then((data) => {
+      console.log(data.data);
+
       data.data.forEach((item, index) => {
         galleryItems[index].querySelector('img').setAttribute('src', item.thumb);
         galleryItems[index].querySelector('.card-text').innerText = item.title;
@@ -36,9 +41,13 @@ function changeTab(index) {
   currentTabIndex = index;
 }
 
+function playBackgroundVideo() {
+  document.querySelector('#heroVideo').src += '&autoplay=1';
+}
+
 sidebarItems.forEach((e, index) => {
   e.addEventListener('click', async () => {
     changeTab(index);
-    await loadData();
+    await loadGallery();
   });
 });
